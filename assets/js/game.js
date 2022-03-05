2e97802b1658654236c7385a79681500c4e6c202
katochsenthal <katoch.senthal@gmail.com>
2022-03-05 10:39:28 -0500

    added fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JS is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+// function name "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+fight();
