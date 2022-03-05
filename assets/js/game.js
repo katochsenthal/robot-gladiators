b8f44fee73e3e2f2bf43d392662e038c3d5c658e
katochsenthal <katoch.senthal@gmail.com>
2022-03-05 11:20:56 -0500

    completed fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,48 @@
-var playerName = window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name? ");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
-// function name "fight"
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-  window.alert("The fight has begun!");
-}
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+console.log(
+  playerName +
+    " attacked " +
+    enemyName +
+    ". " +
+    " Now has " +
+    enemyHealth +
+    " health remaining "
+);
+
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack;
+console.log(
+  enemyName +
+    " attacked " +
+    playerName +
+    ". " +
+    playerName +
+    " now has " +
+    playerHealth +
+    " health remaining "
+);
+
+var fight = function () {
+  window.alert("Robert Gladiators has began! ");
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
 
 fight();
